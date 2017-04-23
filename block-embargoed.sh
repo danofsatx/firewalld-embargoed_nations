cbabc0bf19448264bef92fdde25cdac87659f43f
Dan Mossor <danofsatx@gmail.com>
2017-04-23 19:02:17 -0400

    Now with testing completed! typos/syntax corrected

--- block-embargoed.sh
+++ block-embargoed.sh
@@ -1,4 +1,4 @@
-$!/bin/bash
+#!/bin/bash
 
 #####################################################
 # block_embargoed.sh
@@ -16,8 +16,8 @@ $!/bin/bash
 # Note: North Korea does not have an IPv6 allocation.
 #####################################################
 
-COUNTRYv4=cu ir kp sd sy
-COUNTRYv6=cu ir sd sy
+COUNTRYv4="cu ir kp sd sy"
+COUNTRYv6="cu ir sd sy"
 
 # Create the master lists that everything will be added to
 ipset create ipv4-embargoed_nations list:set
@@ -28,20 +28,20 @@ for i in $COUNTRYv4
   do ipset create $i-embargo hash:net
   for ip in `cat ipv4/$i.zone`
     do ipset add $i-embargo $ip
-    ipset add ipv4-embargoed_nations $i-embargo
   done
+ipset add ipv4-embargoed_nations $i-embargo
 done
 
 for i in $COUNTRYv6
   do ipset create $i-v6-embargo hash:net family inet6
   for ip in `cat ipv6/$i.zone`
-    do ipset add $i-v6-list $ip
-    ipset add ipv6-embargoed_nations $i-embargo
+    do ipset add $i-v6-embargo $ip
   done
+ipset add ipv6-embargoed_nations $i-v6-embargo
 done
 
 # Build the direct rule for firewalld to drop the connections
 # and make it permanent
-firewall-cmd --direct --add-rule 'ipv4 raw PREROUTING_ZONES_SOURCE 0 -m set --match-set ipv4-embargoed_nations src -j DROP'
-firewall-cmd --direct --add-rule 'ipv6 raw PREROUTING_ZONES_SOURCE 0 -m set --match-set ipv6-embargoed_nations src -j DROP'
+firewall-cmd --direct --add-rule ipv4 raw PREROUTING_ZONES_SOURCE 0 -m set --match-set ipv4-embargoed_nations src -j DROP
+firewall-cmd --direct --add-rule ipv6 raw PREROUTING_ZONES_SOURCE 0 -m set --match-set ipv6-embargoed_nations src -j DROP
 firewall-cmd --runtime-to-permanent
